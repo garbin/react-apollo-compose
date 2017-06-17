bd929f6031cea8d96ce4b773382cdd47be104f7a
Garbin <garbinh@gmail.com>
2017-06-17 21:40:01 +0800

    first release

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-import Component from '../../src'
-
-let Demo = React.createClass({
-  render() {
-    return <div>
-      <h1>react-apollo-compose Demo</h1>
-      <Component/>
-    </div>
+class Demo extends React.Component {
+  render () {
+    return <div />
   }
-})
+}
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import { wrap } from 'lodash'
+import { graphql, compose } from 'react-apollo'
 
-export default React.createClass({
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+export function queries (queries, configs = {}) {
+  const hocs = []
+  for (const name in queries) {
+    hocs.push(graphql(queries[name], Object.assign({ name }, configs[name])))
   }
-})
+  return compose(...hocs)
+}
+
+export function mutations (mutations, configs = {}) {
+  const hocs = []
+  const defaultState = {}
+  for (const name in mutations) {
+    hocs.push(graphql(mutations[name], Object.assign({ name }, configs[name])))
+    defaultState[name] = { loading: false }
+  }
+  return compose(...hocs, Component => {
+    return class extends React.Component {
+      constructor (props) {
+        super(props)
+        this.state = defaultState
+      }
+      render () {
+        const props = {}
+        for (const name in mutations) {
+          props[name] = wrap(this.props[name], async (mutate, ...args) => {
+            this.setState({ [name]: {loading: true} })
+            try {
+              const data = await mutate(...args)
+              this.setState({ [name]: {loading: false, data} })
+              return data
+            } catch (error) {
+              this.setState({ [name]: {loading: false, error} })
+              throw error
+            }
+          })
+        }
+        return <Component {...this.props} {...props} mutations={this.state} />
+      }
+    }
+  })
+}
